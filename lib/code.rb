bda59ae5c80c7c9921dbd65d1f86e59fc4fa8666
Spencer Croy <SpencerCCroy@gmail.com>
2019-01-01 15:03:18 -0800

    passing all tests

--- lib/code.rb
+++ lib/code.rb
@@ -1,4 +1,7 @@
+require "byebug"
 class Code
+  attr_reader :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -6,4 +9,58 @@ class Code
     "Y" => :yellow
   }
 
-end
+  def self.valid_pegs?(pegs)
+    pegs.all?{|peg| POSSIBLE_PEGS.keys.include?(peg.upcase)}
+  end
+
+  def initialize(pegs)
+    if Code.valid_pegs?(pegs)
+      @pegs = pegs.map(&:upcase)
+    else
+      raise "Did not submit valid pegs"
+    end
+  end
+
+  def self.random(length)
+    random_pegs = []
+    length.times {random_pegs << POSSIBLE_PEGS.keys.sample}
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(str)
+    Code.new(str.chars)
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(code)
+    count = 0
+    code.pegs.each_with_index do |peg, idx|
+      if peg == @pegs[idx]
+        count += 1
+      end
+    end
+    return count
+  end
+
+  def num_near_matches(code)
+    count = 0
+    code.pegs.each_with_index do |peg, idx|
+      if peg != @pegs[idx] && self.pegs.include?(peg)
+        count += 1
+      end
+    end
+    return count
+  end
+
+
+  def ==(code)
+    num_exact_matches(code) == @pegs.length && @pegs.length == code.length
+  end
+end # Class end
--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+     puts "Exact Matches: #{@secret_code.num_exact_matches(code)}"
+     puts "Near Matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    print "Enter a code"
+    guess_code = Code.from_string(gets.chomp)
+    print_matches(guess_code)
+    return @secret_code == guess_code
+  end
 end
